449aa4c7dfad0b12e382b8c4e0648fb9c4708e72
Nathan O <46687695+N4te2k@users.noreply.github.com>
2023-11-06 10:19:06 +0100

    Initial commit

---
+++ Abfragen/Abfrage_B/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculate
+    {
+        public decimal a;
+        public decimal b;
+        public string op;
+
+        public Calculate() 
+        {
+            a = 0;
+            b = 0;
+            op = string.Empty;
+        }
+         
+        public decimal add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public decimal subtract(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        public decimal multiply(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        public decimal divide(decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+        public int modulo(int a, int b)
+        {
+            return a % b;
+        }
+
+        public decimal logarithmus(decimal a, decimal b)
+        {
+            return (decimal)Math.Log((double)a, (double)b);
+        }   
+
+        public decimal power(decimal a, decimal b)
+        {
+            return (decimal)Math.Pow((double)a, (double)b);
+        }
+    }
+}
---
+++ Abfragen/Abfrage_B/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Calculator;
+
+internal class Program
+{ 
+    public static void Main(string[] args)
+    {
+        Calculate calc = new Calculate();
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Gib eine Zahl ein:");
+                calc.a = decimal.Parse(Console.ReadLine());
+
+                break;
+            }
+            catch (Exception ex) when 
+            (ex is FormatException || ex is ArgumentNullException || ex is OverflowException)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Gib noch eine Zahl ein:");
+                calc.b = decimal.Parse(Console.ReadLine());
+
+                break;
+            }
+            catch (Exception ex) when
+            (ex is FormatException || ex is ArgumentNullException || ex is OverflowException)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        Console.WriteLine("Rechenoperator eingeben:");
+        calc.op = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(calc.op))
+        {
+            switch (calc.op.ToLower())
+            {
+                case "addieren":
+                case "add":
+                case "+":
+                    Console.WriteLine($"Ergebnis: {calc.add(calc.a, calc.b)}");
+                    break;
+
+                case "subtrahieren":
+                case "subtract":
+                case "-":
+                    Console.WriteLine($"Ergebnis: {calc.subtract(calc.a, calc.b)}");
+                    break;
+
+                case "multiplizieren":
+                case "multiply":
+                case "*":
+                    Console.WriteLine($"Ergebnis: {calc.multiply(calc.a, calc.b)}");
+                    break;
+
+                case "dividieren":
+                case "divide":
+                case ":":
+                case "/":
+                    if (calc.b == 0)
+                    {
+                        Console.WriteLine("Division durch 0 nicht möglich!");
+                        return;
+                    }
+
+                    Console.WriteLine($"Ergebnis: {calc.divide(calc.a, calc.b)}");
+
+                    break;
+
+                case "modulo":
+                case "%":
+                    Console.WriteLine($"Ergebnis: {calc.modulo((int)calc.a, (int)calc.b)}");
+                    break;
+
+                case "logarithmus":
+                case "log":
+                    Console.WriteLine($"Ergebnis: {calc.logarithmus(calc.a, calc.b)}");
+                    break;
+
+                case "power":
+                case "pow":
+                case "^":
+                    Console.WriteLine($"Ergebnis: {calc.power(calc.a, calc.b)}");
+                    break;
+            }
+        }
+    }
+}
